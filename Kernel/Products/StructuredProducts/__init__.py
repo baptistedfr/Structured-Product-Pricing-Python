c48716fb10477f2c266b9338a3fe60d5eb3a77af
baptistedfr <bapdufo@gmail.com>
2025-03-20 16:19:04 +0100

    Update __init__.py

--- Kernel/Products/StructuredProducts/__init__.py
+++ Kernel/Products/StructuredProducts/__init__.py
@@ -1,6 +1,6 @@
-from abstract_structured_product import AbstractStructuredProduct
+from .abstract_structured_product import AbstractStructuredProduct
 
-from participation_products import (
+from .participation_products import (
     AbstractParticipationProduct,
     TwinWin,
     Airbag
